fe478fc933706d09b17296c0e275d0d0b8db5112
IconicTelomere <Rockingsplash4@gmail.com>
2024-11-19 14:45:49 +1100

    Created the register and login pages in auth folder

--- src/components/common/SiteNav.js
+++ src/components/common/SiteNav.js
@@ -10,7 +10,8 @@ function SiteNav() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-md-auto">
-                            <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/register">Register</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
